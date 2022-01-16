396c40e671a9f91571cb96e84285faf8e7eda349
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-01-16 10:54:07 +0100

    chore: setup amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "smartshelffrontend0a68706f": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
