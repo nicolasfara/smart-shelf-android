b50021d539560803dd0cc1a4b86f4a03dfc363ff
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-01-27 22:14:09 +0100

    feat: listProducts query

---
+++ app/src/main/java/it/unibo/sc/ListProductsQuery.kt
@@ -0,0 +1,53 @@
+package it.unibo.sc
+
+import android.util.Log
+import com.amplifyframework.api.ApiException
+import com.amplifyframework.api.aws.ApiGraphQLRequestOptions
+import com.amplifyframework.api.aws.AppSyncGraphQLRequest
+import com.amplifyframework.api.graphql.GraphQLRequest
+import com.amplifyframework.api.graphql.PaginatedResult
+import com.amplifyframework.api.graphql.QueryType
+import com.amplifyframework.datastore.generated.model.Product
+import com.amplifyframework.kotlin.core.Amplify
+import com.amplifyframework.util.TypeMaker
+
+/**
+ *
+ */
+class ListProductsQuery() {
+    /**
+     *
+     */
+    suspend fun getProducts(nextToken: String?): PaginatedResult<Product>? {
+        return listProducts(nextToken, 2)
+    }
+
+    private suspend fun listProducts(nextToken: String?, limit: Int): PaginatedResult<Product>? {
+        return try {
+            val res = Amplify.API.query(getProductsRequest(nextToken, limit))
+            Log.d("ListProductsQuery", "Query succeeded")
+            res.data
+        } catch (error: ApiException) {
+            Log.e("ListProductsQuery", "Query failed", error)
+            null
+        }
+    }
+
+    private fun getProductsRequest(
+        nextToken: String?,
+        limit: Int
+    ): GraphQLRequest<PaginatedResult<Product>> {
+        val responseType = TypeMaker.getParameterizedType(
+            PaginatedResult::class.java,
+            Product::class.java
+        )
+        return AppSyncGraphQLRequest.builder()
+            .modelClass(Product::class.java)
+            .operation(QueryType.LIST)
+            .responseType(responseType)
+            .variable("limit", "Int", limit)
+            .variable("nextToken", "String", nextToken)
+            .requestOptions(ApiGraphQLRequestOptions())
+            .build()
+    }
+}
--- app/src/main/java/it/unibo/sc/viewmodel/ProductsViewModel.kt
+++ app/src/main/java/it/unibo/sc/viewmodel/ProductsViewModel.kt
@@ -1,35 +1,24 @@
 package it.unibo.sc.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
-import com.amplifyframework.api.ApiException
-import com.amplifyframework.api.graphql.model.ModelQuery
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.liveData
 import com.amplifyframework.datastore.generated.model.Product
-import com.amplifyframework.kotlin.core.Amplify
+import it.unibo.sc.ListProductsQuery
+import it.unibo.sc.ProductsPagingSource
 
 /**
  *
  */
 class ProductsViewModel : ViewModel() {
+    private val listProductsQuery = ListProductsQuery()
     /**
      *
      */
-    fun products(): LiveData<List<Product>> = liveData {
-        val data = listProducts()
-        emit(data)
-    }
-
-    /**
-     *
-     */
-    private suspend fun listProducts(): List<Product> {
-        return try {
-            Amplify.API.query(ModelQuery.list(Product::class.java)).data.items.toList()
-        } catch (error: ApiException) {
-            Log.e("ProductsViewModel", "Query failed", error)
-            emptyList()
-        }
-    }
+    fun products(): LiveData<PagingData<Product>> = Pager(PagingConfig(pageSize = 5)) {
+        ProductsPagingSource(listProductsQuery)
+    }.liveData
 }
