a6df8d3fad32f0206daa063c69996bd615b448b5
Linda Vitali <linda.vitali2@studio.unibo.it>
2022-02-04 16:49:35 +0100

    feat: nfc layout

--- app/src/main/java/it/unibo/sc/activities/ProductActivity.kt
+++ app/src/main/java/it/unibo/sc/activities/ProductActivity.kt
@@ -1,6 +1,8 @@
 package it.unibo.sc.activities
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import it.unibo.sc.databinding.ActivityProductBinding
 
@@ -16,6 +18,10 @@ class ProductActivity : AppCompatActivity() {
 
         val writeTagButton = binding.writeTagButton
         val deleteProductButton = binding.deleteProductButton
+
+        writeTagButton.setOnClickListener {
+            changeUItoNFCView()
+        }
     }
 
     private fun displayProductInfo() {
@@ -26,5 +32,14 @@ class ProductActivity : AppCompatActivity() {
         productNameText.text = intent.extras?.get("productName").toString()
         productPriceText.text = intent.extras?.get("productPrice").toString()
         productExpiringDateText.text = intent.extras?.get("productExpiringDate").toString()
+    }
+
+    private fun changeUItoNFCView() {
+        val card = binding.cardView
+        val productInfoLayout = binding.productInfoLayout
+        val nfcLayout = binding.nfcLayout
+        card.setCardBackgroundColor(Color.rgb(61, 220, 132))
+        nfcLayout.visibility = View.VISIBLE
+        productInfoLayout.visibility = View.INVISIBLE
     }
 }
